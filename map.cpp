87f15b52e319892cb6cc72134e6b58f0fae9db11
Kevin <33016327+min3gyu@users.noreply.github.com>
2018-02-08 23:13:50 -0600

    map constructing files.

---
+++ map.cpp
@@ -0,0 +1,23 @@
+#include "map.hpp"
+using namespace std;
+map::map(){
+	start_X = 0;
+	start_Y = 0;
+	goal_X = 0; 
+	goal_Y = 0; 
+	width = 0; 
+	height = 0;
+}
+
+map::map(const &map other){
+	start_X = other.get_start_X();
+	start_Y = other.get_start_Y();
+	goal_X = other.get_goal_X(); 
+	goal_Y = other.get_goal_Y(); 
+	width = other.get_width(); 
+	height = other.get_height();
+}
+
+bool load_map(string filename){
+
+}
---
+++ map.hpp
@@ -0,0 +1,21 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+class map{
+	private:
+		int start_X, start_Y, goal_X, goal_Y, width, height;
+	public:
+		map();
+		map(const &map other);
+		bool load_map(string filename);
+		int get_start_X();
+		int get_start_Y();
+		int get_goal_X();
+		int get_goal_Y();
+		int get_width();
+		int get_height();
+};
